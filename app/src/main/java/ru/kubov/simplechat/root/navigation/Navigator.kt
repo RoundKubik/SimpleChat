603a05a45087fa2fe0652c9198571af7bc41f666
RoundKubik <kubov.a.nikita@gmail.com>
2021-09-02 08:52:36 +0300

    added base navigator methods

--- app/src/main/java/ru/kubov/simplechat/root/navigation/Navigator.kt
+++ app/src/main/java/ru/kubov/simplechat/root/navigation/Navigator.kt
@@ -1,8 +1,34 @@
 package ru.kubov.simplechat.root.navigation
 
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+
 
 /**
  * Entity implements navigation between, fragments
  * **/
 class Navigator {
+
+    private var navController: NavController? = null
+    private var activity: AppCompatActivity? = null
+
+    /**
+     *  Method provides attaching navigation controller [NavController] and activity [AppCompatActivity]
+     *
+     *  @param navController - main navigation controller
+     *  @param activity - activity for navigation
+     */
+    fun attach(navController: NavController, activity: AppCompatActivity) {
+        this.navController = navController
+        this.activity = activity
+    }
+
+    /**
+     *  Method provides detach navigation controller [NavController] and activity [AppCompatActivity]
+     *
+     */
+    fun detach() {
+        navController = null
+        activity = null
+    }
 }
