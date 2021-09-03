f617c92e79365a3961aa4d06d58379c0d6f4c951
RoundKubik <kubov.a.nikita@gmail.com>
2021-09-03 21:01:15 +0300

    Added fragment implementation for profile

---
+++ core-utils/src/main/java/ru/kubov/core_utils/domain/Profile.kt
@@ -0,0 +1,7 @@
+package ru.kubov.core_utils.domain
+
+data class Profile(
+    val id: Long,
+    val nickName: String,
+    val online: Boolean
+)
--- feature-main-impl/src/main/java/ru/kubov/feature_profile_impl/presentation/profile/ProfileFragment.kt
+++ feature-main-impl/src/main/java/ru/kubov/feature_profile_impl/presentation/profile/ProfileFragment.kt
@@ -1,6 +1,76 @@
 package ru.kubov.feature_profile_impl.presentation.profile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.kubov.feature_profile_impl.R
+import ru.kubov.feature_profile_impl.databinding.FragmentProfileBinding
+import ru.kubov.feature_profile_impl.databinding.IncludeProfileMenuOptionBinding
 
+
+/**
+ * Class implements ui of user profile
+ */
 class ProfileFragment : Fragment() {
+
+     //@Inject
+     lateinit var viewModel: ProfileViewModel
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    private var _includeEditProfileBinding: IncludeProfileMenuOptionBinding? = null
+    private val includeEditProfileBinding get() = _includeEditProfileBinding!!
+
+    private var _includeThemeBinding: IncludeProfileMenuOptionBinding? = null
+    private val includeThemeBinding get() = _includeThemeBinding!!
+
+    private var _includeDataAndStorageBinding: IncludeProfileMenuOptionBinding? = null
+    private val includeDataAndStorageBinding get() = _includeDataAndStorageBinding!!
+
+    private var _includeLogoutBinding: IncludeProfileMenuOptionBinding? = null
+    private val includeLogoutBinding get() = _includeLogoutBinding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        inject()
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        _includeEditProfileBinding = IncludeProfileMenuOptionBinding.bind(binding.frgProfileIncludeEditProfile.root)
+        _includeThemeBinding = IncludeProfileMenuOptionBinding.bind(binding.frgProfileIncludeTheme.root)
+        _includeDataAndStorageBinding =
+            IncludeProfileMenuOptionBinding.bind(binding.frgProfileIncludeDataAndStorage.root)
+        _includeLogoutBinding = IncludeProfileMenuOptionBinding.bind(binding.frgProfileIncludeLogout.root)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initMenuOptions()
+        subscribe()
+    }
+
+    private fun initMenuOptions() {
+        includeEditProfileBinding.includeProfileMenuOptionsIvIcon.setImageResource(R.drawable.ic_edit_24)
+        includeEditProfileBinding.includeProfileMenuOptionsTvTitle.text = getString(R.string.edit_profile)
+
+        includeThemeBinding.includeProfileMenuOptionsIvIcon.setImageResource(R.drawable.ic_choice_theme_24)
+        includeThemeBinding.includeProfileMenuOptionsTvTitle.text = getString(R.string.theme)
+
+        includeDataAndStorageBinding.includeProfileMenuOptionsIvIcon.setImageResource(R.drawable.ic_data_24)
+        includeDataAndStorageBinding.includeProfileMenuOptionsTvTitle.text = getString(R.string.data_and_storage)
+
+        includeLogoutBinding.includeProfileMenuOptionsIvIcon.setImageResource(R.drawable.ic_door_24)
+        includeLogoutBinding.includeProfileMenuOptionsTvTitle.text = getString(R.string.exit_from_profile)
+    }
+
+    private fun subscribe() {
+        viewModel.profile.observe(this.viewLifecycleOwner) {
+            
+        }
+    }
+
+    private fun inject() {
+
+    }
 }
