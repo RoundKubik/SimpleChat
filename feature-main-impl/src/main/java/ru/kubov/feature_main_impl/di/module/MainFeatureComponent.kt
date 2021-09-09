b5c2eb3354c0e9307e1d14158bf9a3ca55b2fd7a
RoundKubik <kubov.a.nikita@gmail.com>
2021-09-09 06:28:51 +0300

    [POINT] - added providing main feature router in di

--- feature-main-impl/src/main/java/ru/kubov/feature_main_impl/di/module/MainFeatureComponent.kt
+++ feature-main-impl/src/main/java/ru/kubov/feature_main_impl/di/module/MainFeatureComponent.kt
@@ -1,9 +1,12 @@
 package ru.kubov.feature_main_impl.di.module
 
+import android.app.AppComponentFactory
+import dagger.BindsInstance
 import dagger.Component
 import ru.kubov.core_utils.di.PerFeature
 import ru.kubov.feature_main_api.di.MainFeatureApi
 import ru.kubov.feature_main_api.di.MainFeatureDependencies
+import ru.kubov.feature_main_api.navigation.MainRouter
 import ru.kubov.feature_main_impl.di.presentation.edit_profile.EditProfileComponent
 import ru.kubov.feature_main_impl.di.presentation.profile.ProfileComponent
 import ru.kubov.feature_main_impl.di.presentation.settings_data_and_storage.SettingsDataAndStorageComponent
@@ -23,7 +26,7 @@ interface MainFeatureComponent : MainFeatureApi {
     @Component.Factory
     interface Factory {
         fun create(
-            deps: MainFeatureDependencies
+            mainFeatureDeps: MainFeatureDependencies
         ): MainFeatureComponent
     }
 
--- feature-main-impl/src/main/java/ru/kubov/feature_main_impl/di/module/MainFeatureComponentHolder.kt
+++ feature-main-impl/src/main/java/ru/kubov/feature_main_impl/di/module/MainFeatureComponentHolder.kt
@@ -7,9 +7,10 @@ import ru.kubov.module_injection.holder.ComponentHolder
 object MainFeatureComponentHolder : ComponentHolder<MainFeatureApi, MainFeatureDependencies>() {
 
     override fun initializeDependencies(): MainFeatureApi {
-        val mainFeatureDependencies = DaggerMainFeatureComponent_MainFeatureDependenciesComponent.builder()
-            .build()
+        val mainFeatureDependencies =
+            DaggerMainFeatureComponent_MainFeatureDependenciesComponent.builder().build()
 
-        return DaggerMainFeatureComponent.factory().create(mainFeatureDependencies)
+        return DaggerMainFeatureComponent.factory()
+            .create(mainFeatureDependencies)
     }
 }
