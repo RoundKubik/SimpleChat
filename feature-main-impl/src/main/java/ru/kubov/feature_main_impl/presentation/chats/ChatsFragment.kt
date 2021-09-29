a790845367cc7adf1526b8e25786f144bdf941f8
RoundKubik <kubov.a.nikita@gmail.com>
2021-09-29 06:19:59 +0300

    Added inflation of layout in chats fragment

--- feature-main-impl/src/main/java/ru/kubov/feature_main_impl/presentation/chats/ChatsFragment.kt
+++ feature-main-impl/src/main/java/ru/kubov/feature_main_impl/presentation/chats/ChatsFragment.kt
@@ -1,10 +1,37 @@
 package ru.kubov.feature_main_impl.presentation.chats
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.kubov.feature_main_impl.databinding.FragmentChatsBinding
+import ru.kubov.feature_main_impl.databinding.IncludeBaseToolbarBinding
+import ru.kubov.feature_main_impl.databinding.IncludeMainToolbarBinding
 import ru.kubov.feature_main_impl.di.module.MainFeatureComponent
 import ru.kubov.feature_main_impl.di.module.MainFeatureComponentHolder
 
+// TODO: 29.09.2021
 class ChatsFragment : Fragment() {
+
+    private var _binding: FragmentChatsBinding? = null
+    private val binding get() = _binding!!
+
+    private var _include: IncludeMainToolbarBinding? = null
+    private val include get() = _include!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        inject()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentChatsBinding.inflate(inflater, container, false)
+        _include = IncludeMainToolbarBinding.bind(binding.frgChatsIncludeToolbar.root)
+        return binding.root
+    }
+
+
 
     private fun inject() {
         (MainFeatureComponentHolder.get() as MainFeatureComponent)
