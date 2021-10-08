af6453e42331b0c33973b0b0ab5701cec91e9393
RoundKubik <kubov.a.nikita@gmail.com>
2021-10-08 16:27:52 +0300

    Moved binding form chat adapter with using ChatView.kt

--- core-ui/src/main/java/com/kubov/core_ui/presentation/chats/adapter/ChatsAdapter.kt
+++ core-ui/src/main/java/com/kubov/core_ui/presentation/chats/adapter/ChatsAdapter.kt
@@ -1,22 +1,20 @@
 package com.kubov.core_ui.presentation.chats.adapter
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.kubov.core_ui.databinding.ItemChatInfoBinding
+import com.kubov.core_ui.presentation.view.chat.ChatView
 import ru.kubov.core_utils.domain.models.Chat
-import ru.kubov.core_utils.extensions.setDebounceClickListener
-import ru.kubov.core_utils.extensions.showImage
 
 // TODO: 02.10.2021 add documentation
-open class ChatsAdapter(private val onChatClick: ((Chat?) -> Unit)? = null) : ListAdapter<Chat, ChatsAdapter.ChatViewHolder>(
-    ChatDiffCallback()
-) {
+open class ChatsAdapter(private val onChatClick: ((Chat?) -> Unit)? = null) :
+    ListAdapter<Chat, ChatsAdapter.ChatViewHolder>(
+        ChatDiffCallback()
+    ) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
-        val binding = ItemChatInfoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ChatViewHolder(binding)
+        val view = ChatView(parent.context)
+        return ChatViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
@@ -24,11 +22,11 @@ open class ChatsAdapter(private val onChatClick: ((Chat?) -> Unit)? = null) : Li
     }
 
     inner class ChatViewHolder(
-        val binding: ItemChatInfoBinding,
-    ) : RecyclerView.ViewHolder(binding.root) {
+        val view: ChatView
+    ) : RecyclerView.ViewHolder(view) {
 
         init {
-            binding.root.setDebounceClickListener {
+            view.onChatClickListener = {
                 val position = adapterPosition
                 if (position != RecyclerView.NO_POSITION) {
                     onChatClick?.invoke(getItem(position))
@@ -37,7 +35,7 @@ open class ChatsAdapter(private val onChatClick: ((Chat?) -> Unit)? = null) : Li
         }
 
         fun bindChat(chat: Chat) {
-
+            view.showChat(chat)
         }
     }
 }
